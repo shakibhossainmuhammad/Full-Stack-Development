56f050e25c46993b097e87edc12a6879a6e4284d
muhammadshakibhossain <shakibhossainmuhammad@gmail.com>
2022-09-13 19:56:27 +0600

    Full Stack Develoment

---
+++ Web Development/12-Getting-Started-With-JavaScript/1-Variables.js
@@ -0,0 +1,52 @@
+// var bananaPrice = 12;
+// console.log(typeof bananaPrice);
+
+// var seenAfter = 21;
+// console.log(typeof seenAfter);
+
+// var name = 'KolimUddin Zuckerburg';
+// console.log(typeof name);
+
+// var isHot = true;
+// var isRich = false;
+// console.log(typeof isHot);
+
+// var myName = 'Hero Alom';
+// MyName = 'Hero abdul';
+
+// console.log(myName);
+
+// var promise = 'I promise i will work to become a programmer';
+// // console.log(promise.toUpperCase());
+// console.log(promise.split(' '));
+
+// var number1 = 0.1;
+// var number2 = 0.2;
+// number2 = parseFloat(number2);
+// number2 = ' ' + number1;
+// var total = number1 + number2;
+// total = total.toFixed(1);
+// console.log(total);
+
+// class 9
+
+// var price1 = 25;
+// var price2 = 35;
+// price2--;
+// console.log(price2);
+// var price1 = 'Justin';
+// var name = 'Biebar';
+// var result = price1 + ' ' + name;
+// console.log(result);
+
+// class 10
+
+// var number = 5.0;
+// var result = Math.round(number);
+// console.log(result);
+// var number = 15.00000009;
+// var result = Math.random(number);
+// console.log(result);
+var number = Math.random() * 100;
+var result = Math.round(number);
+console.log(result);
---
+++ Web Development/12-Getting-Started-With-JavaScript/2-conditionals.js
@@ -0,0 +1,18 @@
+// var biscutPrice = 7;
+
+// if (biscutPrice != 8) {
+//   console.log('I will eat the biscuites');
+// } else {
+//   console.log('Khali cha is better');
+// }
+
+var jobPaiso = false;
+var savingAmount = 500;
+
+if (jobPaiso == true && savingAmount > 200000) {
+  console.log('Aso tomar patri khuji');
+} else if (jobPaiso == true) {
+  console.log('accha koi din pore dekhbo');
+} else {
+  console.log('Tor kopale biya nai');
+}
