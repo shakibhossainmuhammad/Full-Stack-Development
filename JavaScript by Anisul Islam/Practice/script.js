d8fe3def31c8aacbbc72937f590f47d0e30e9968
muhammadshakibhossain <shakibhossainmuhammad@gmail.com>
2022-12-20 18:36:34 +0600

    Array

---
+++ JavaScript by Anisul Islam/Practice/script.js
@@ -0,0 +1,39 @@
+// if, else if, switch
+
+// let num1 = parseInt(prompt('Enter first Number : '));
+// let num2 = parseInt(prompt('Enter second Number : '));
+
+// if (num1 > num2) {
+//   console.log('Large Number is : ' + num1);
+//   console.log('Large Number is : ' + num1);
+// }
+// if (num1 < num2) {
+//   console.log('Large Number is : ' + num2);
+//   console.log('Large Number is : ' + num2);
+// }
+
+// if (num1 == num2) {
+//   console.log('Equal Numbers');
+// }
+
+// if (num1 > num2) {
+//   console.log('Large Number is : ' + num1);
+// } else if (num1 < num2) {
+//   console.log('large Number is : ' + num2);
+// } else {
+//   console.log('Equal Numbers');
+// }
+
+// convert any uppercase input into lower cause we set only lowercase letter in condition
+
+letter = letter.toLowerCase();
+
+if (
+  letter == 'a' ||
+  letter == 'e' ||
+  letter == 'i' ||
+  letter == 'o' ||
+  letter == 'u'
+) {
+  console.log('Vowel');
+}
