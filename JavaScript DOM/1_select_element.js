47d7edf40664a334c040db06678bf1c13d081cc3
Muhammad Shakib Hossain <shakibhossainmuhammad@gmail.com>
2022-12-28 01:54:50 +0600

    Update JavaScript Dom select element & onclick event

---
+++ JavaScript DOM/1_select_element.js
@@ -0,0 +1,14 @@
+// let ID1 = document.getElementById('ID1');
+// ID1.innerHTML = 'I Love My Country!';
+
+// let className1 = document.getElementsByClassName('className1')[3];
+
+// className1.innerHTML = 'Hello Shakib!';
+
+// let Headings = document.getElementsByName('Headings')[0];
+// Headings.innerHTML = 'Headings 5';
+
+// let H1 = document.getElementsByTagName('h1')[0];
+// H1.innerHTML = 'Heading One Is Changed';
+
+document.querySelectorAll('h1');
---
+++ JavaScript DOM/git_push.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -a -m "Update JavaScript Dom select element & onclick event"
+git push
